8d19cf4548d6d43358e378c619c6958f081264a8
Aarusha Thakral <aarusha.thakral@empire.ca>
2017-10-27 15:01:43 -0400

    Front-end styling changes

--- webpack/app/javascripts/app.js
+++ webpack/app/javascripts/app.js
@@ -2,7 +2,7 @@
 import "../stylesheets/app.css";
 
 // Import libraries we need.
-import { default as Web3} from 'web3';
+import { default as Web3 } from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
@@ -15,138 +15,160 @@ var MetaCoin = contract(metacoin_artifacts);
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
 var accounts;
-var account;
+var admin_account;
 
-window.App = {
-  start: function() {
-    var self = this;
+// Hard-coded member names. The first member owns contracts by default (hence Admin).
+var member_names = ['Admin', 'Emma', 'Aarusha', 'Andrew', 'Mark', 'Tanvir', 'Tuan', 'Luke', 'Alice', 'Bob'];
+// Associative array to link names to account hashes
+var account_hashes = {};
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+window.createMemberDropdowns = function(member_list) {
+    var $option;
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
+    for(var i=0; i < member_list.length; i++) {
+        $option = $("<option>", { "value": account_hashes[member_list[i]].toString() }).text(member_list[i]);
+        $(".member_select").append($option);
+    }
+}
 
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
-
-      accounts = accs;
-      account = accounts[0];
-
-      self.refreshBalance();
-    });
-  },
-
-  setStatus: function(message) {
+window.setStatus = function(message) {
     var status = document.getElementById("status");
-    status.innerHTML = message;
-  },
+    status.innerHTML = "Status: " + message;
+}
 
-  refreshBalance: function() {
-    var self = this;
-
+window.refreshAdminBalance = function() {
     var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
-  },
-
-  showBalance: function() {
-    var self = this;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
-  },
-
-  sendCoin: function() {
-    var self = this;
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
-    });
-  },
-
-getBalance: function() {
-    var self = this;
-
-    var address = document.getElementById("id_address").value;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(address, {from: address});
-    }).then(function(value) {
-      document.getElementById("balance_div").innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error retrieving balance; see log.");
-    });
-},
-
-freeMoney: function() {
-    var self = this;
-
-    var address = document.getElementById("id_free_money_address").value;
-    var meta;
-    
     MetaCoin.deployed().then(function(instance) {
         meta = instance;
-        return meta.freeMoney(address, {from: address});
+        return meta.getBalance.call(admin_account, { from: admin_account });
+    }).then(function(value) {
+        var balance_element = document.getElementById("balance");
+        balance_element.innerHTML = value.valueOf();
+    }).catch(function(e) {
+        console.log(e);
+        setStatus("Error getting balance; see log.");
+    });
+}
+
+window.showBalance = function() {
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+        meta = instance;
+        return meta.getBalance.call(admin_account, { from: admin_account });
+    }).then(function(value) {
+        var balance_element = document.getElementById("balance_span");
+        balance_element.innerHTML = "Balance = " + value.valueOf() + " META";
+    }).catch(function(e) {
+        console.log(e);
+        setStatus("Error getting balance; see log.");
+    });
+}
+
+window.sendCoin = function() {
+    var amount = parseInt(document.getElementById("amount").value);
+    var receiver = document.getElementById("id_receiver").value;
+    
+    if (receiver == admin_account) {
+        setStatus("Error - Cannot send money to yourself");
+        return;
+    }
+
+    setStatus("Initiating transaction... (please wait)");
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+        meta = instance;
+        return meta.sendCoin(receiver, amount, { from: admin_account });
+    }).then(function() {
+        setStatus("Transaction complete!");
+        refreshAdminBalance();
+    }).catch(function(e) {
+        console.log(e);
+        setStatus("Error sending coin; see log.");
+    });
+}
+
+window.getBalance = function() {
+    var member = document.getElementById("id_member").value;
+
+    var balance_element = document.getElementById("balance_span");
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+        meta = instance;
+        return meta.getBalance.call(member, { from: member });
+    }).then(function(value) {        
+        balance_element.innerHTML = "Balance = " + value.valueOf() + " META";
+    }).catch(function(e) {
+        balance_element.innerHTML = e;
+        setStatus("Error retrieving balance; see log.");
+    });
+}
+
+window.freeMoney = function() {
+    var address = document.getElementById("id_recepient").value;
+    var meta;
+
+    MetaCoin.deployed().then(function(instance) {
+        meta = instance;
+        return meta.freeMoney(address, { from: address });
     }).then(function(value) {
         document.getElementById("free_money_div").innerHTML = "Money added: " + value.valueOf();
     }).catch(function(e) {
         console.log(e);
-        self.setStatus("Error retrieving balance; see log.");
+        setStatus("Error retrieving balance; see log.");
     });
 }
-};
 
-window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  }
+window.printAccounts = function() {
+    try {
+        for (var key in account_hashes) {
+            $("#account_hashes").append(key + ": " + account_hashes[key] + "<br/>");
+        }
+    }
+    catch(err) {
+        $("#account_hashes_status").text(err.message);
+    }
+}
 
-  App.start();
+$(document).ready(function() {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+        // Use Mist/MetaMask's provider
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+
+    // Bootstrap the MetaCoin abstraction for Use.
+    MetaCoin.setProvider(web3.currentProvider);
+
+    var self = this;
+    // Get the initial account balance so it can be displayed.
+    web3.eth.getAccounts(function(err, accs) {
+        if (err != null) {
+            alert("There was an error fetching your accounts.");
+            return;
+        }
+
+        if (accs.length == 0) {
+            alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+            return;
+        }
+
+        accounts = accs;
+        admin_account = accounts[0];
+
+        // Assign account hashes to member names
+        for (var i = 0; i < member_names.length; i++) {
+            account_hashes[member_names[i]] = accounts[i];
+        }
+
+        refreshAdminBalance();
+
+        createMemberDropdowns(member_names);
+
+    }); 
 });
