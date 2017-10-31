d9c1c96b57da0b07eec3fbe1451f26e943b0f7ba
Aarusha Thakral <aarusha.thakral@empire.ca>
2017-10-31 09:41:11 -0400

    Added bootstrap to index page

--- webpack/launch.sh
+++ webpack/launch.sh
@@ -6,15 +6,36 @@
 # truffle migrate
 # npm run dev
 #
-#
+
+if [ -d "~/tmp" ]; then
+	TEMP_DIR = ~/tmp
+else
+	TEMP_DIR = /tmp
+fi
+
+TEMP_FILE = $TEMP_DIR/printfile.$$.$RANDOM 
+
+cleanup() {
+	exit $1
+}
+
+error_exit() {
+	echo "${PROGNAME}: ${1:-"Error"}" 1>&2
+	cleanup 1
+}
+
+trap cleanup SIGHUP SIGINT SIGTERM
+
 echo 'Running truffle compile...'
-truffle compile
+truffle compile || error_exit "truffle compile failed. exiting...\n"
 echo
 
-echo 'Running truffle migrate...'
-truffle migrate
+'Running truffle migrate...'
+truffle migrate || error_exit "truffle migrate failed. exiting...\n"
 echo
 
 echo 'Starting server with npm run dev...'
-npm run dev
+npm run dev || error_exit "server failed to start. exiting...\n"
 echo
+
+cleanup
